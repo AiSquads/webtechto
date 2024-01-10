550630b14f3fb5a00a3384590193f4f08fdb87e3
AiSquads <collegematterzz@gmail.com>
2024-01-10 10:55:31 +0530

    first commit

---
+++ server/controller/controller.js
@@ -0,0 +1,8 @@
+const db = require('../database/database')
+const express = require('express')
+const bp = require('body-parser')
+
+const app = express();
+app.use(bp.urlencoded({ extended: false }));
+
+app.use(bp.json())
