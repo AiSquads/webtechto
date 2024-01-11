f1270b0ea77222573cb4257bc6b5c199a4f04004
AiSquads <149037635+AiSquads@users.noreply.github.com>
2024-01-11 13:46:09 +0530

    new change for iBOY

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -11,3 +11,19 @@ app.use(bp.json())
 exports.index = async (req, res) => {
     res.render('index.ejs', { title: 'Tectovia' })
 };
+
+exports.contact = async (req, res) => {
+    res.render('contact.ejs', { title: 'Contact' })
+};
+
+exports.about_us = async (req, res) => {
+    res.render('about_us.ejs', { title: 'About' })
+};
+
+exports.sign_in = async (req, res) => {
+    res.render('sign_in.ejs', { title: 'Sign In' })
+};
+
+exports.sign_up = async (req, res) => {
+    res.render('sign_up.ejs', { title: 'Sign Up' })
+};
--- server/router/router.js
+++ server/router/router.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const controller = require('../controller/controller')
 
 router.get('/',controller.index);
+router.get('/contact',controller.contact);
+router.get('/about_us',controller.about_us);
+router.get('/sign_in',controller.sign_in);
+router.get('/sign_up',controller.sign_up);
+
 
 
 module.exports = router;
